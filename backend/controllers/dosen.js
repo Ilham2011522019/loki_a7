86046290fd9af52691511fd354b3a7f0cd3babc2
Ilham2011522019 <2011522019@student.unand.ac.id>
2022-06-30 16:04:59 +0700

    ilham

--- backend/controllers/dosen.js
+++ backend/controllers/dosen.js
@@ -5,18 +5,6 @@ const cookieParser = require('cookie-parser')
 
 const controllers = {}
 
-controllers.home = async(req, res) => {
-    const accessToken = req.cookies.accessToken 
-     if (!accessToken)
-         return res.status(200).json("tidak ada token")
-     const payload = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET)
-     const nama = payload.nama
-     const NIP = payload.NIP
- 
-     res.render("dosen_dashbord", {accessToken, nama, NIP} )
-     // res.json({RPS})
- }
-
 controllers.matkul = async(req, res) => {
    const accessToken = req.cookies.accessToken 
     if (!accessToken)
